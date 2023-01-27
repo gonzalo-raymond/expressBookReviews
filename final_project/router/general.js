7102b1a815341492aa8ed0021bebe53e8a3e4626
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-27 12:43:24 -0500

    Adding all the public gets

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,7 +10,6 @@ function insertSpaces(string) {
     return string;
 }
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -70,8 +69,21 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const isbn = req.params.isbn;
+
+    if(books[isbn]){
+
+        if(books[isbn].review){
+            res.send(books[isbn].review);
+        }else{
+            res.status(404).json({message: "No reviews yet."});
+        }
+
+    }else{
+        res.status(404).json({message: "The isbn is incorrect."});
+    }
+
 });
 
 module.exports.general = public_users;
