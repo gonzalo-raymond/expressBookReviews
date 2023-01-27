56a606c9aa1ab4c956b5d62b1d76a63541845938
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-27 12:13:20 -0500

    Final Project backend

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,12 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function insertSpaces(string) {
+    string = string.replace(/([a-z])([A-Z])/g, '$1 $2');
+    string = string.replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+    return string;
+}
+
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,26 +18,54 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+
+    const isbn = req.params.isbn;
+
+    if(books[isbn]){
+        res.send(books[isbn]);
+    }else{
+        res.status(404).json({message: "The ISBN code is incorrect or the book does not exist."})
+    }
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const author = insertSpaces(req.params.author);
+
+    let isbns = Object.keys(books);
+
+    let filtered_index = isbns.filter((isbn) =>  books[isbn].author === author);
+
+    if(filtered_index.length > 0){
+        res.send(books[filtered_index]);
+    }else{
+        res.status(404).json({message: "The author does not exist."});
+    }
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const title = insertSpaces(req.params.title);
+
+    let isbns = Object.keys(books);
+
+    let filtered_index = isbns.filter((isbn) =>  (books[isbn].title) === title);
+
+    if(filtered_index.length > 0){
+        res.send(books[filtered_index]);
+    }else{
+        res.status(404).json({message: "The title does not exist."});
+    }
+
 });
 
 //  Get book review
