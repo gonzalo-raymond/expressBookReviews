e8c631dfe8d72c35153cab35adedc007a85add3c
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-01-28 09:10:58 -0500

    Implement async await to the getters

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 let books = require("./booksdb.js");
 let isAvailable = require("./auth_users.js").isAvailable;
 let users = require("./auth_users.js").users;
+const { response } = require('express');
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
@@ -24,73 +25,95 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
 
-
-
-    res.send(JSON.stringify({books}, null, 4));
+    try{
+        res.send(JSON.stringify({books}, null, 4));
+    }catch(error){
+        res.status(404).send(error);
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
 
     const isbn = req.params.isbn;
 
-    if(books[isbn]){
-        res.send(books[isbn]);
-    }else{
-        res.status(404).json({message: "The ISBN code is incorrect or the book does not exist."})
+    try{
+
+        if(books[isbn]){
+            res.send(books[isbn]);        
+        }else{
+            res.status(404).json({message: "The ISBN code is incorrect or the book does not exist."});
+        }
+
+    }catch(error){
+        res.status(404).send(error);
     }
 
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
 
     const author = req.params.author;
 
-    let isbns = Object.keys(books);
+    try{
 
-    let filtered_index = isbns.filter((isbn) =>  books[isbn].author === author);
+        let isbns = Object.keys(books);
 
-    if(filtered_index.length > 0){
-        res.send(books[filtered_index]);
-    }else{
-        res.status(404).json({message: "The author does not exist."});
-    }
+        let filtered_index = isbns.filter((isbn) =>  books[isbn].author === author);
+
+        if(filtered_index.length > 0){
+            res.send(books[filtered_index]);
+        }else{
+            res.status(404).json({message: "The author does not exist."});
+        }
+
+    }catch(error){
+        res.status(404).send(error);
+    } 
 
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
 
     const title = req.params.title;
 
-    let isbns = Object.keys(books);
+    try{
 
-    let filtered_index = isbns.filter((isbn) =>  (books[isbn].title) === title);
+        let isbns = Object.keys(books);
 
-    if(filtered_index.length > 0){
-        res.send(books[filtered_index]);
-    }else{
-        res.status(404).json({message: "The title does not exist."});
+        let filtered_index = isbns.filter((isbn) =>  (books[isbn].title) === title);
+
+        if(filtered_index.length > 0){
+            res.send(books[filtered_index]);
+        }else{
+            res.status(404).json({message: "The title does not exist."});
+        }
+
+    }catch(error){
+        res.status(404).send(error);
     }
-
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', async function (req, res) {
 
     const isbn = req.params.isbn;
 
-    if(books[isbn]){
+    try{
 
-        res.send(books[isbn].reviews);
+        if(books[isbn]){
+            res.send(books[isbn].reviews);
+        }else{
+            res.status(404).json({message: "The isbn is incorrect."});
+        }
 
-    }else{
-        res.status(404).json({message: "The isbn is incorrect."});
+    }catch(error){
+        es.status(404).send(error);
     }
-
 });
 
 module.exports.general = public_users;
